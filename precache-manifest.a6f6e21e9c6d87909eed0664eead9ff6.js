7dfa72c80072e752d87666091ec849cfc26982f9
Matt Jones <mjones@softwaredesignsolutions.com>
2019-03-19 17:02:24 -0400

    Updates

---
+++ precache-manifest.a6f6e21e9c6d87909eed0664eead9ff6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/daily-prayer/static/media/logo.ee7cd8ed.svg"
+  },
+  {
+    "revision": "63055bf283c9d2d7a9a1",
+    "url": "/daily-prayer/static/js/runtime~main.4d1eff03.js"
+  },
+  {
+    "revision": "0253ce8350a3ae2f114f",
+    "url": "/daily-prayer/static/js/main.cdc740a0.chunk.js"
+  },
+  {
+    "revision": "ba264cd90c4d36546d5a",
+    "url": "/daily-prayer/static/js/2.8f71a45e.chunk.js"
+  },
+  {
+    "revision": "0253ce8350a3ae2f114f",
+    "url": "/daily-prayer/static/css/main.b26158c1.chunk.css"
+  },
+  {
+    "revision": "43b9ca0827577a163a6c4f67061fd247",
+    "url": "/daily-prayer/index.html"
+  }
+];
