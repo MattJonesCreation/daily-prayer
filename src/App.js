ec123038bd2358ac3bc16bc8576584c791a12e79
Matt Jones <mjones@softwaredesignsolutions.com>
2019-03-20 11:57:26 -0400

    Setting up the main page of the site for daily-prayer

--- src/App.js
+++ src/App.js
@@ -1,17 +1,28 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = { value: '' };
+    }
+
+    handleChange = (event) => {
+        this.setState({ value: event.target.value });
+    }
+
+    handleSubmit = (event) => {
+        this.setState({ value: '' });
+        event.preventDefault();
+    }
+
     render() {
         return (
             <div className="App">
                 <header className="App-header">
-                    <img src={logo} className="App-logo" alt="logo" />
-                    <p>Edit <code>src/App.js</code> and save to reload.</p>
-                    <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-                        Learn React
-                    </a>
+                    <p>Send your prayer to God.</p>
+                    <textarea className="App-textarea" value={this.state.value} onChange={this.handleChange} />
+                    <button className="App-button" onClick={this.handleSubmit}>Send</button>
                 </header>
             </div>
         );
